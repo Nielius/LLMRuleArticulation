0ece100b8d97051610a04e748c1229ebd05079f7
Niels uit de Bos <nielius@gmail.com>
2023-12-02 15:36:20 +0000

    Create main.py

--- src/rule_articulation/main.py
+++ src/rule_articulation/main.py
@@ -1,4 +1,27 @@
 import logging
 
+import typer
+
+from rule_articulation.evaluator import TaskEvaluator
+from rule_articulation.tasks.capitalization import (
+    CapitalizationDataset,
+    capitalization_task,
+)
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
+
+
+app = typer.Typer()
+
+
+@app.command()
+def capitalization(n: int = 10, seed: int = 42):
+    dataset = CapitalizationDataset()
+
+    evaluation_report = TaskEvaluator().evaluate(capitalization_task, dataset.sample(n))
+    evaluation_report.print()
+
+
+if __name__ == "__main__":
+    app()
